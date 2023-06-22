be9409e825a4a9cc449a26281e2928579962147a
Jasmine Tai <jtai@jtai.ca>
2023-06-21 17:47:20 -0700

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,13 +5,43 @@
 </script>
 
 <header>
-    <div class="navbar bg-primary text-primary-content">
+    <nav class="navbar bg-primary text-primary-content">
         <a href="/" class="btn btn-ghost">Home</a>
         <a href="/projects" class="btn btn-ghost">Projects</a>
         <a href="/contact" class="btn btn-ghost">Contact</a>
-    </div>
+    </nav>
 </header>
 
-<main class="max-w-[42rem] mx-auto my-10 px-4">
+<main class="my-10 px-4">
     <slot />
 </main>
+
+<footer class="bg-base-200 text-base-content">
+    <div class="footer p-8">
+        <div>
+            <span class="footer-title">Links</span>
+            <a href="https://github.com/j-tai">GitHub</a>
+        </div>
+        <div>
+            <span class="footer-title">About</span>
+            <a href="/contact">Contact</a>
+            <a href="https://github.com/j-tai/jtai.ca">Source code</a>
+        </div>
+        <div>
+            <span class="footer-title">Webring</span>
+            <a href="https://se-webring.xyz/">SE Webring</a>
+        </div>
+    </div>
+</footer>
+
+<style lang="postcss">
+    main,
+    footer > div {
+        @apply mx-auto;
+        max-width: 42rem;
+    }
+
+    footer a {
+        @apply link link-hover;
+    }
+</style>
