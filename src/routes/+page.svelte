72258bc03477f6649b281cd921ca47c1734c4c3d
Jasmine Tai <jtai@jtai.ca>
2023-06-21 18:55:16 -0700

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,14 @@
 
     <p>
         Hi there! I'm Jasmine (she/her), and I'm a software engineering student at the University of
-        Waterloo. I'm passionate about software and tech, from FPGAs to web development.
+        Waterloo. I'm passionate about everything software and tech, from FPGAs to web development.
     </p>
     <p>
-        I learned coding in 2014, starting with Python, and later, Java. I've been using Linux as my
-        daily driver since 2017. I've been running my a home Linux server since 2018, which hosts
-        whatever I need. My favourite languages are Rust and Python. In my free time, I like doing
-        web development and modding video games.
+        I learned coding in 2014, starting with Python, then Java, then a whole bunch of other
+        languages. I've been using Linux as my daily driver since 2017, and I've also been running a
+        home Linux server since 2018. My favourite languages are Rust and Python. In my free time, I
+        like developing web apps, modding video games, and playing the piano.
     </p>
+
+    <p>Check out some of my <a href="/projects">projects</a>.</p>
 </div>
