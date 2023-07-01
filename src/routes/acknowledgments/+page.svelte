4b82caa3b0d5a6d578368caf304516c44d3cde69
Jasmine Tai <jtai@jtai.ca>
2023-06-30 18:54:34 -0700

    Add font to acknowledgments page

--- src/routes/acknowledgments/+page.svelte
+++ src/routes/acknowledgments/+page.svelte
@@ -7,8 +7,16 @@
 
 <div class="text">
     <p>
+        This website uses the <a href="https://www.ibm.com/plex/">IBM Plex font<ExternalLink /></a>,
+        licensed under <a href="https://scripts.sil.org/OFL">OFL-1.1<ExternalLink /></a>.
+    </p>
+
+    <p>
         This website uses icons from the <a href="https://carbondesignsystem.com/"
             >Carbon Design System<ExternalLink /></a
+        >, licensed under the
+        <a href="https://github.com/carbon-design-system/carbon/blob/main/LICENSE"
+            >Apache 2.0 license<ExternalLink /></a
         >.
     </p>
 
