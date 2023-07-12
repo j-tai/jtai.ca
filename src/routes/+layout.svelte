42f6fadebd6629329fffce2a93f2999316e0b5e5
Jasmine Tai <jtai@jtai.ca>
2023-07-11 19:01:02 -0700

    Add Mastodon link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,6 +30,7 @@
         <div>
             <span class="footer-title">Links</span>
             <a href="https://github.com/j-tai">GitHub<ExternalLink /></a>
+            <a rel="me" href="https://tech.lgbt/@jtai">Mastodon<ExternalLink /></a>
         </div>
         <div>
             <span class="footer-title">About</span>
