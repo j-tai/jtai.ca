6ed14a5965b34a9f8a0e010fa972efe803412218
Jasmine Tai <jtai@jtai.ca>
2023-08-27 15:03:55 -0700

    Fix external link icon stretching text height

--- src/lib/ExternalLink.svelte
+++ src/lib/ExternalLink.svelte
@@ -4,4 +4,10 @@
     export let size: 16 | 20 | 24 | 32 = 16;
 </script>
 
-<Launch class="inline ml-[0.25em] mb-[0.4em]" {size} {...$$restProps} />
+<Launch class="inline ml-[0.25em] shift-up" {size} {...$$restProps} />
+
+<style lang="postcss">
+    :global(.shift-up) {
+        transform: translateY(-0.2em);
+    }
+</style>
