b9caf80abf9149be2ed7077fc212cd4bfa27b9c6
Jasmine Tai <jtai@jtai.ca>
2023-08-27 15:13:46 -0700

    Fix external link icon positioning

--- src/lib/ExternalLink.svelte
+++ src/lib/ExternalLink.svelte
@@ -4,10 +4,11 @@
     export let size: 16 | 20 | 24 | 32 = 16;
 </script>
 
-<Launch class="inline ml-[0.25em] shift-up" {size} {...$$restProps} />
+<Launch class="inline ml-[0.25em] inline-icon" {size} {...$$restProps} />
 
 <style lang="postcss">
-    :global(.shift-up) {
-        transform: translateY(-0.2em);
+    :global(.inline-icon) {
+        vertical-align: baseline;
+        transform: translateY(0.2rem);
     }
 </style>
