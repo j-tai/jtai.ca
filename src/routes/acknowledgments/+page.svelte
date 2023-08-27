698251e668795f60da913f4ca1e56d6d4a5053a9
Jasmine Tai <jtai@jtai.ca>
2023-08-27 15:13:53 -0700

    Add Delius font to acknowledgments page

--- src/routes/acknowledgments/+page.svelte
+++ src/routes/acknowledgments/+page.svelte
@@ -7,23 +7,25 @@
 
 <div class="text">
     <p>
-        This website uses the <a href="https://www.ibm.com/plex/">IBM Plex font<ExternalLink /></a>,
-        licensed under <a href="https://scripts.sil.org/OFL">OFL-1.1<ExternalLink /></a>.
+        This website uses the
+        <a href="https://fonts.google.com/specimen/Delius/about">Delius<ExternalLink /></a>
+        and <a href="https://www.ibm.com/plex/">IBM Plex<ExternalLink /></a> fonts, both licensed
+        under <a href="https://scripts.sil.org/OFL">OFL-1.1<ExternalLink /></a>.
     </p>
 
     <p>
-        This website uses icons from the <a href="https://carbondesignsystem.com/"
-            >Carbon Design System<ExternalLink /></a
-        >, licensed under the
+        This website uses icons from the
+        <a href="https://carbondesignsystem.com/">Carbon Design System<ExternalLink /></a>, licensed
+        under the
         <a href="https://github.com/carbon-design-system/carbon/blob/main/LICENSE"
             >Apache 2.0 license<ExternalLink /></a
         >.
     </p>
 
     <p>
-        The website favicon is adapted from <a href="https://openmoji.org/"
-            >OpenMoji<ExternalLink /></a
-        >, the open-source emoji and icon project, licensed under
+        The website favicon is adapted from
+        <a href="https://openmoji.org/">OpenMoji<ExternalLink /></a>, the open-source emoji and icon
+        project, licensed under
         <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0<ExternalLink /></a>.
     </p>
 </div>
