2f32056c998c9192d0ddfb30282bbe470625346a
Jasmine Tai <jtai@jtai.ca>
2023-09-04 18:17:42 -0400

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,9 @@
     </p>
     <p>
         I first learned to code in 2014, starting with Python, then Java, then a whole bunch of
-        other languages. I've been using Linux as my daily driver since 2017, and I've also been
-        running a home Linux server since 2018. My favourite languages are Rust and Python. During
-        my free time, I like to develop web apps, mod video games, play the piano, and crochet.
+        other languages. I've also been running a home Linux server since 2018. My favourite
+        languages are Rust and Python. During my free time, I like to develop web apps, mod video
+        games, play the piano, and crochet.
     </p>
 
     <p>Check out some of my <a href="/projects">projects</a>.</p>
