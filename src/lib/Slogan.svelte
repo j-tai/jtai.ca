75c19ee6a4d024953429b282dabb8cb2c3e03c60
Jasmine Tai <jtai@jtai.ca>
2024-12-25 17:18:23 -0800

    Implement remaining of home page

--- src/lib/Slogan.svelte
+++ src/lib/Slogan.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-    import { onMount, type ComponentProps } from 'svelte';
+    import { type HTMLAttributes } from 'svelte/elements';
     import { type FlipParams } from 'svelte/animate';
     import { quadInOut } from 'svelte/easing';
-    import { fly } from 'svelte/transition';
-    import Heading from './Heading.svelte';
     import { partialFlip } from './partial-flip';
 
-    let props: Omit<ComponentProps<typeof Heading>, 'children'> = $props();
-
-    let init = $state(false);
-    onMount(() => (init = true));
+    let { class: classes, ...props }: HTMLAttributes<HTMLHeadingElement> = $props();
 
     let hovering = $state(false);
     let alternate = $state(false);
@@ -17,13 +12,13 @@
     let lines = $derived.by(() => {
         if (hovering !== alternate) {
             return [
-                { key: 1, words: ['art', '\u00a0', 'is'] },
-                { key: 0, words: ['software'] },
+                { key: 1, words: ['Art', '\u00a0', 'is'] },
+                { key: 0, words: ['Software'] },
             ];
         } else {
             return [
-                { key: 0, words: ['software'] },
-                { key: 1, words: ['is', '\u00a0', 'art'] },
+                { key: 0, words: ['Software'] },
+                { key: 1, words: ['is', '\u00a0', 'Art'] },
             ];
         }
     });
@@ -34,8 +29,9 @@
     };
 </script>
 
-<Heading
-    align="right"
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<h1
+    class="heading text-right {classes}"
     {...props}
     onclick={() => (alternate = !alternate)}
     onpointerenter={(e) => {
@@ -51,18 +47,13 @@
         }
     }}
 >
-    {#if init}
-        {#each lines as line (line.key)}
-            <div
-                animate:partialFlip={{ y: true, ...flipParams }}
-                in:fly|global={{ y: -200, duration: 750 }}
-            >
-                {#each line.words as word (word)}
-                    <span class="inline-block" animate:partialFlip={{ x: true, ...flipParams }}>
-                        {word}
-                    </span>
-                {/each}
-            </div>
-        {/each}
-    {/if}
-</Heading>
+    {#each lines as line (line.key)}
+        <div animate:partialFlip={{ y: true, ...flipParams }}>
+            {#each line.words as word (word)}
+                <span class="inline-block" animate:partialFlip={{ x: true, ...flipParams }}>
+                    {word}
+                </span>
+            {/each}
+        </div>
+    {/each}
+</h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,40 @@ export default {
     ],
 
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                'fly-above': 'fly-above 1.5s ease-out',
+                'fly-below': 'fly-below 1.5s ease-out',
+                'fly-left': 'fly-left 1.5s ease-out',
+                'fly-right': 'fly-right 1.5s ease-out',
+                fade: 'fade 1.5s ease-in-out',
+            },
+
+            keyframes: (() => {
+                const fly = (translation: string) => ({
+                    '0%, 35%': {
+                        translate: translation,
+                        opacity: '0',
+                    },
+                    to: {
+                        translate: '0 0',
+                        opacity: '1',
+                    },
+                });
+
+                return {
+                    'fly-above': fly('0 -40svh'),
+                    'fly-below': fly('0 40svh'),
+                    'fly-left': fly('-40svw 0'),
+                    'fly-right': fly('40svw 0'),
+
+                    fade: {
+                        '0%, 50%': { opacity: '0' },
+                        to: { opacity: '1' },
+                    },
+                };
+            })(),
+        },
     },
 
     plugins: [
