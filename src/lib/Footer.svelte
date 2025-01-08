b40ea2f4be95b4e533eda6b24b4f1c360cccbb76
Jasmine Tai <jtai@jtai.ca>
2025-01-08 17:39:33 -0500

    Add SE webring to footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,8 @@
 <footer class="flex flex-row flex-wrap justify-center bg-surface-200 lowercase">
-    <a href="https://en.pronouns.page/@jtai">Pronouns.page</a>
-    <a href="https://github.com/j-tai">GitHub</a>
+    <a href="https://en.pronouns.page/@jtai" target="_blank">Pronouns.page</a>
+    <a href="https://github.com/j-tai" target="_blank">GitHub</a>
+    <a href="https://se-webring.xyz" target="_blank">SE Webring</a>
+    <a href="https://github.com/j-tai/jtai.ca" target="_blank">Source code</a>
 </footer>
 
 <style lang="postcss">
